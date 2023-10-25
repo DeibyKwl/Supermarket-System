2083709cb49d283019bf2084824e5d787ef689a0
deibywu <deiby.wu@maine.edu>
2023-10-24 22:59:58 -0400

    Added more functionalities
    You can retrieve all items or an specific item

--- item_json.py
+++ item_json.py
@@ -2,12 +2,15 @@ import json
 
 file_path = 'item.json'
 
-# Code to make sure there is a json file
-try:
-    with open(file_path, 'r') as json_file:
-        json_data = json.load(json_file)
-except FileNotFoundError:
-    json_data = {}
+# Code to make sure there is a json file, if not create an empty json file
+def file_checker():
+    try:
+        with open(file_path, 'r') as json_file:
+            json_data = json.load(json_file)
+    except FileNotFoundError:
+        json_data = {}
+    
+    return json_data
 
 # Add the new data to the json_file.
 # Example of how to use it: #add_item(12345, 'Arroz', 12.32, '3F', json_data)
@@ -50,8 +53,23 @@ def update_item(barcode, name, price, section, quantity):
         json.dump(json_data, json_file, indent=4)
 
 
+# Return all the items in the json database
+def retrieve_all_items():
+    return json_data
+
+# Return specific item by barcode
+def retrieve_item(barcode):
+    return json_data[str(barcode)]
+
+
+json_data = file_checker()
+
+
 
 # UNIT TESTING
-#add_item(98652, 'Arroz', 12.32, '3F', json_data)
-#remove_item(23048)
-#update_item(98652, 'Queso', 50.10, '9C', 10)
+#barcode = 98652
+#add_item(barcode, 'Arroz', 12.32, '3F', json_data)
+#remove_item(barcode)
+#update_item(barcode, 'Queso', 50.10, '9C', 10)
+#all_items = retrieve_all_items()
+#item = retrieve_item(barcode)
