f011c0b1637981de3df686db1d6837867683c077
deibywu <deiby.wu@maine.edu>
2023-10-26 19:37:59 -0400

    Delete staff_controller.py

--- staff_controller.py
+++
@@ -1,8 +0,0 @@
-from json_script import staff_json
-
-def choices():
-    print('press 1 to add a staff')
-    print('press 2 to remove a staff')
-    print('press 3 to update a staff')
-    print('press 4 to view staff member')
-    print('Press \'q\' to quit') 
