07b8b78ee79e9d864558ed5f554723c6eed067ef
deibywu <deiby.wu@maine.edu>
2023-10-26 19:37:51 -0400

    Create top_level_view_item.py

---
+++ top_level_view_item.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+
+from item_services import *
+
+def open_new_window():
+
+    new_window = tk.Tk()
+    new_window.title('Settings')
+    new_window.geometry('1000x700')
+    new_window.focus_force()
+
+    cancel_button = tk.Button(new_window, text='Cancel', command=new_window.destroy, width=20, height=5)
+    cancel_button.place(x=840,y=610)
+    item_services(new_window)
+    item_buttons(new_window)
+    new_window.mainloop()
+
+
+def item_services(new_window):
+    item_label = tk.Label(new_window, text='Items', font=('Helvetica',16))
+    item_label.place(x=20,y=20)
+
+def item_buttons(new_window):
+    item_button_add = tk.Button(new_window, text='Add Items', command=add_item, width=20, height=5)
+    item_button_add.place(x=20, y=50)
