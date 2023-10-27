78abb5daac4db1e89eea7ccc1bfc5ed72f65efa0
deibywu <deiby.wu@maine.edu>
2023-10-26 20:28:07 -0400

    Update item_view.py

--- item_view.py
+++ item_view.py
@@ -14,7 +14,7 @@ root.title('Supermarket System')
 root.geometry('1280x720')
 
 #Setting Buttons
-setting_button = tk.Button(root, text='Setting', command=top_level_view_item.open_new_window, width=20, height=5)
+setting_button = tk.Button(root, text='Setting', command=settings_view.open_new_window, width=20, height=5)
 setting_button.place(x=10,y=630)
 
 
